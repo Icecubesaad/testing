23030e928469c16fb265e4bd6be2b456c1c62895
Icecubesaad <saad.qad418@gmail.com>
2023-05-27 01:11:58 +0500

    wtfbab

--- api/router/blogs.js
+++ api/router/blogs.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
 const blogs = require("../model/blogs")
-const except = { Image: 0, Description: 0 };
-  
+
 router.get("/get",async(req,res)=>{
+    const except = { Image: 0, Description: 0 };
     const data = await blogs.find({},except).limit(4)
+    res.send(data)
 })
